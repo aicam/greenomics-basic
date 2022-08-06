1a37e0bf905b4a146f441b5e65c2c50c9d23d35a
ali <a_icam@yahoo.com>
2022-08-06 12:19:30 -0700

    revision 1

---
+++ components/LandingPage/HowWorks.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "HowWorks"
+}
+</script>
+
+<style scoped>
+
+</style>
--- components/LandingPage/OurGoal.vue
+++ components/LandingPage/OurGoal.vue
@@ -1,36 +1,20 @@
 <template>
   <v-container fluid style="background: white" class="text-center">
     <h1 style="padding: 25px; color: black">Our Services in One Glance</h1>
-    <v-row justify="center" align="center">
-      <v-col lg="4">
+    <v-row justify="center" align="center" class="ma-2">
+      <v-col class="pa-3 d-flex flex-column" style="padding: 20px" v-for="(item, i) in cardsInfo" :key="i">
         <v-row justify="center">
           <v-card
-            style="background: white"
+            style="background: white;"
             elevation="10"
-            max-width="350"
+            max-width="400"
+            height="450"
           >
-            <img class="tile" src="@/static/dao.png" height="200px"/>
+            <img style="padding: 10px" class="tile" :src="item.img" height="200px" max-width="400px"/>
             <v-card-text>
-              <h3 style="color: #672f08; font-size: 35px; margin-top: 20px; font-family: 'DejaVu Sans'">Verification</h3>
-              <p style="color: black; font-size: 20px; padding-top: 30px;">How do you know the technology or the project
-                is real and remove carbon? Blockchain provides the most transparent and easiest way.</p>
-            </v-card-text>
-          </v-card>
-        </v-row>
-      </v-col>
-
-      <v-col lg="4">
-        <v-row justify="center">
-          <v-card
-            style="background: white"
-            elevation="10"
-            max-width="350"
-          >
-            <img class="tile" src="@/static/dao.png" height="200px"/>
-            <v-card-text>
-              <h3 style="color: #672f08; font-size: 35px; margin-top: 20px; font-family: 'DejaVu Sans'">Investment</h3>
-              <p style="color: black; font-size: 20px; padding-top: 30px;">How do you know the technology or the project
-                is real and remove carbon? Blockchain provides the most transparent and easiest way.</p>
+              <h3 style="color: #672f08; font-size: 35px; margin-top: 20px; font-family: 'DejaVu Sans';
+                  letter-spacing: 3px">{{item.title}}</h3>
+              <p style="color: black; font-size: 20px; padding-top: 30px;">{{item.desc}}</p>
             </v-card-text>
           </v-card>
         </v-row>
@@ -40,8 +24,36 @@
 </template>
 
 <script>
+import daoImg from '../../static/dao.png';
+import investmentImg from '../../static/landing/investment.png'
+import projectImg from '../../static/landing/project.png'
+import smartcontractImg from '../../static/landing/smart_contract.png'
+
 export default {
   name: "OurGoal",
+  data() {
+    return({
+      cardsInfo: [
+        {'title': 'Verification', 'img': daoImg,
+          'desc': 'How do you know the technology or the project' +
+            ' is real and remove carbon? Blockchain provides the most transparent and easiest way.'},
+        {'title': 'Investment', 'img': investmentImg,
+          'desc': 'There are many companies that need to emit carbon to produce,' +
+            ' many people who want to contribute in Carbon removing and ... . Blockchain provides a transparent system to invest' +
+            ' and see who has invested on what.'},
+        {
+          'title': 'Project', 'img': projectImg,
+          'desc': 'There 7+ billion people looking for inventions that save us from global warming! you have an idea? define your' +
+            ' project and get your own coin, make money, save the world'
+        },
+        {
+          'title': 'Smart Contract', 'img': smartcontractImg,
+          'desc': 'There are many regulations in different countries that are exhausting and parties should comply them. We' +
+            ' deploy them all as smart contracts so you don\'t need be worried about them'
+        }
+      ]
+    })
+  },
   mounted() {
     window.addEventListener('scroll', fadeIn );
     let elementsArray = document.querySelectorAll(".tile");
