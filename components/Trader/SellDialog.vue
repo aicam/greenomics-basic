1ec5474e13458c4f1ad14fd1722e0e1017fae331
ali <a_icam@yahoo.com>
2022-09-05 08:27:52 -0700

    revision 2

--- components/Trader/SellDialog.vue
+++ components/Trader/SellDialog.vue
@@ -5,12 +5,18 @@
     @click:outside="$emit('close')"
     width="400px"
   >
-    <v-card>
-      <v-card-title class="text-center">
-        You are selling stock from {{info.nft.company_name}}
-      </v-card-title>
+    <v-card style="padding: 25px; background: rgba(86,70,144,0.76)">
+      <h3>You are selling stock from {{ info.nft.company_name }}</h3>
+      <v-spacer></v-spacer>
+      <v-container>
+        <v-row justify="center" align="center" style="padding-top: 25px;">
+          <v-text-field :label="`You have ${info.stock} stock`" v-model="amount"></v-text-field>
+          <v-btn @click="amount = info.stock">Max</v-btn>
+        </v-row>
+      </v-container>
       <v-card-actions>
         <v-btn @click="$emit('close')">Close</v-btn>
+        <v-btn @click="sell">Sell</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -19,7 +25,19 @@
 <script>
 export default {
   name: "SellDialog",
-  props: ['dialog', 'info']
+  props: ['dialog', 'info'],
+  data() {
+    return {
+      amount: null
+    }
+  },
+  methods: {
+    sell() {
+      this.$axios.get("http://localhost:8000/marketplace/sell?id=" + this.info.id + "&amount=" + this.amount).then(() =>
+        location.reload()
+      )
+    }
+  }
 }
 </script>
 
